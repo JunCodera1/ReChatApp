535e45ab76030f760c7c1ef1c68136c035077048
JunCodera1 <minhtien0601200@gmail.com>
2024-08-04 12:29:48 +0700

    Message UI update part 1

---
+++ src/controller/ButtonController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class ButtonController {
+
+    @FXML
+    void ButtonClicked(MouseEvent event) {
+    	System.out.println("Enter");
+    }
+
+}
